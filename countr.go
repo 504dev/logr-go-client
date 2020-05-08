9a0035b31344a9b6397a25b79c2b1b8f4f5011f4
Mona Lisa <nikruzhok@gmail.com>
2020-05-08 14:05:34 +0300

    .Time

--- countr.go
+++ countr.go
@@ -109,15 +109,5 @@ func (ctr *Counter) Per(key string, taken float64, total float64) *types.Count {
 }
 
 func (ctr *Counter) Time(key string, d time.Duration) func() time.Duration {
-	ts := time.Now()
-	var delta *time.Duration
-	return func() time.Duration {
-		if delta == nil {
-			tmp := time.Since(ts)
-			delta = &tmp
-			num := float64(time.Since(ts).Nanoseconds()) / float64(d.Nanoseconds())
-			ctr.touch(key).Avg(num).Min(num).Max(num)
-		}
-		return *delta
-	}
+	return ctr.touch(key).Time(d)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/504dev/logr-go-client
 go 1.13
 
 require (
-	github.com/504dev/kidlog v0.0.0-20200313221337-e1ebab884b48
+	github.com/504dev/kidlog v0.0.2
 	github.com/fatih/color v1.9.0
 )
